b2ddbb48a2692a20e1c2402cd590aeb2fed261f1
Andrei Alexandru Scoicaru <133790348+Andu02@users.noreply.github.com>
2025-07-09 18:58:47 +0300

    Update db.js for Render and Neon

--- server/db.js
+++ server/db.js
@@ -3,12 +3,23 @@ import config from "./config.js";
 
 const { Pool } = pg;
 
-const pool = new Pool({
-  user: config.db.user,
-  host: config.db.host,
-  database: config.db.database,
-  password: config.db.password,
-  port: config.db.port,
-});
+let pool;
+
+if (config.databaseUrl) {
+  pool = new Pool({
+    connectionString: config.databaseUrl,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+} else {
+  pool = new Pool({
+    user: config.db.user,
+    host: config.db.host,
+    database: config.db.database,
+    password: config.db.password,
+    port: config.db.port
+  });
+}
 
 export default pool;
